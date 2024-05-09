961a30303162d746796cd260f7ad66c3d0289542
Okafor Emeka <emekaicthub@gmail.com>
2024-05-09 13:39:07 +0100

    adding footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  
+     return (
+    <footer className="bg-indigo-700 border-b border-indigo-500 p-5">
+          <div className='container text-white text-center text-xl mx-auto'>
+              <p>React &copy; 2024</p>
+         </div>
+    </footer>
+  );
+  
+}
+
+export default Footer
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,13 +2,15 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Footer from '../components/Footer';
 
 const MainLayout = () => {
   return (
       <>
           <Navbar />
          <Outlet />
-         <ToastContainer />
+      <ToastContainer />
+      <Footer />
       
     </>
   )
