fa3050d3f60dc72d450c1ccb5d9d6c24023309f9
Okafor Emeka <emekaicthub@gmail.com>
2024-05-09 10:56:28 +0100

    second commit

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "https://react-jobs-t7pm.onrender.com",
         changeOrigin: true,
-        rewrite: (path) =>path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-})
+});
